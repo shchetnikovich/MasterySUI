b928154045e5c99e401e8bf609cac086f26bc6b1
shchetnikovich <shchetnikovich@gmail.com>
2023-01-21 16:10:46 +0300

    Initial Commit

---
+++ MasterySUI/MasterySUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasterySUIApp.swift
+//  MasterySUI
+//
+//  Created by Anton Shchetnikovich on 21.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct MasterySUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
